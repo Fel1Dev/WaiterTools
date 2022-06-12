c665dbd871e238eeae2ab381ca4db0fd7570d490
Andres Torres <andrestor2@gmail.com>
2022-06-12 18:26:03 -0500

    feat/frontend: Added new component and css details.

--- waiter-tools/src/app/component/delivery-processor/delivery-processor.component.ts
+++ waiter-tools/src/app/component/delivery-processor/delivery-processor.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-delivery-processor',
   templateUrl: './delivery-processor.component.html',
-  styleUrls: ['./delivery-processor.component.css']
+  styleUrls: ['./delivery-processor.component.css'],
 })
 export class DeliveryProcessorComponent implements OnInit {
+  deliveryProcessor!: FormGroup;
+  loading = false;
+  submitted = false;
+  error = '';
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {}  
 
   ngOnInit(): void {
+    this.deliveryProcessor = this.formBuilder.group({
+      dateProcess: ['', Validators.required],
+    });
   }
 
+  get f() {
+    return this.deliveryProcessor.controls;
+  }
+
+  OnSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.deliveryProcessor.invalid) {
+      return;
+    }
+    this.loading = true;
+    //Call report process
+    //Change flag to show delivery-viewer component
+  }
 }
