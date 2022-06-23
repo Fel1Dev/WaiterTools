ef7cc049fa41e8bdc2b44f3b0f181516410e1959
Andres Torres <andrestor2@gmail.com>
2022-06-22 20:59:28 -0500

    feat/frontend: added new service and partial logic

--- waiter-tools/src/app/component/delivery-viewer/delivery-viewer.component.ts
+++ waiter-tools/src/app/component/delivery-viewer/delivery-viewer.component.ts
@@ -1,26 +1,42 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { DeliveryDataService } from 'src/app/services';
+import * as moment from 'moment';
+import { AuthenticationService, DeliveryDataService } from 'src/app/services';
 
 @Component({
   selector: 'app-delivery-viewer',
   templateUrl: './delivery-viewer.component.html',
-  styleUrls: ['./delivery-viewer.component.css']
+  styleUrls: ['./delivery-viewer.component.css'],
 })
 export class DeliveryViewerComponent implements OnInit {
-
   @Input() visible!: boolean;
   @Input() dateSelected!: string;
-  constructor(private deliveryDataService: DeliveryDataService   ) { }
+  constructor(
+    private deliveryDataService: DeliveryDataService,
+    private authenticationService: AuthenticationService
+  ) {}
 
   ngOnInit(): void {
     console.log('Domi Viewer');
-    if (this.dateSelected) {
-
-    }
+    this.getRecords();
   }
 
   getRecords() {
+    if (this.dateSelected) {
+      const tokenValue = this.authenticationService.tokenValue;
+      const restaurantId =  tokenValue.restaruantId;
+      this.deliveryDataService.getCallcenterReport(this.dateSelected, restaurantId)
+      .subscribe(
+        {
+          next: (response: any) => {
+            console.table(response);
+          },
+          error: () => {
+            //this.error = this.error;
+            //this.loading = false;
+          },
+        }
+      );
 
+    }
   }
-
 }
--- waiter-tools/src/app/services/delivery-data.service.ts
+++ waiter-tools/src/app/services/delivery-data.service.ts
@@ -1,5 +1,6 @@
-import { HttpClient, HttpParamsOptions } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import * as moment from 'moment';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -8,13 +9,25 @@ import { environment } from 'src/environments/environment';
 export class DeliveryDataService {
   constructor(private http: HttpClient) {}
 
-  getCallcenterReport(selectedDate: Date, restaurantId: string) {
+  getCallcenterReport(selectedDate: string, restaurantId: string) {
     const callcenterUrl = `${environment.BASE_URL}${environment.CALLCENTER_PATH}`;
-    const httpOptions: HttpParamsOptions = {};
-    //Query parametesr
-    
+
+    const startTime = moment(selectedDate).unix();
+    const endTime = moment(selectedDate).add(1, 'day').unix();
+
+    console.log('path:' + callcenterUrl);
+    console.log('startTime:' + startTime);
+    console.log('endTime:' + endTime);
+
+    const params: HttpParams = new HttpParams();
+    params.set('restaurantId', restaurantId);
+    params.set('startTime', startTime);
+    params.set('endTime', endTime);
+    params.set('requestType', false);
+
+    const options = { params: params };
+
     //Create model with values to represent correctly the structure.
-    this.http.get<any>(callcenterUrl, {});
-    //return all
+    return this.http.get<any>(callcenterUrl, options);
   }
 }
