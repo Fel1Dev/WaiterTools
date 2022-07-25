c32224daf6962558e5cba30e34626de5dee25851
Andres Torres <andrestor2@gmail.com>
2022-07-24 23:53:26 -0500

    fix environment prod values

--- waiter-tools/src/environments/environment.prod.ts
+++ waiter-tools/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  BASE_URL: 'https://sleepy-coast-01312.herokuapp.com/',
+  AUTH_PATH: 'api/v1/authentication',
+  DELIVERY_PATH: 'api/v1/reports/delivery'
 };
