b403a2a99aa122c68da042a35e1696f56940d640
Andres Torres <andrestor2@gmail.com>
2022-12-04 10:35:44 -0500

    Hotfix: modify backend service to use render platform

--- waiter-tools/src/environments/environment.prod.ts
+++ waiter-tools/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  BASE_URL: 'https://sleepy-coast-01312.herokuapp.com/',
+  BASE_URL: 'https://waitertools-services.onrender.com/',
   AUTH_PATH: 'api/v1/authentication',
   DELIVERY_PATH: 'api/v1/reports/delivery'
 };
